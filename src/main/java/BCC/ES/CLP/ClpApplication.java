45aeba99d28091474fb5193d061cd5da33288ab4
claujunior <claudivanjunior2002@hotmail.com>
2026-03-25 11:47:09 -0300

    inicio projeto

---
+++ src/main/java/BCC/ES/CLP/ClpApplication.java
@@ -0,0 +1,13 @@
+package BCC.ES.CLP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClpApplication.class, args);
+	}
+
+}
