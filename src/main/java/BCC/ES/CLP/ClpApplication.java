f6360a6adb25cea287688cbd8f0bf4534e668137
claujunior <claudivanjunior2002@hotmail.com>
2026-04-14 19:01:32 -0300

    certo

--- src/main/java/BCC/ES/CLP/ClpApplication.java
+++ src/main/java/BCC/ES/CLP/ClpApplication.java
@@ -1,5 +1,6 @@
 package BCC.ES.CLP;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClpApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 
-
+		dotenv.entries().forEach(e ->
+				System.setProperty(e.getKey(), e.getValue())
+		);
 		SpringApplication.run(ClpApplication.class, args);
 	}
 
