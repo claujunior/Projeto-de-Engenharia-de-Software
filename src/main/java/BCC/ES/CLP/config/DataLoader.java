ee187f6525a1bd0f11c79f4f056f86471c55dffa
claujunior <claudivanjunior2002@hotmail.com>
2026-04-14 18:25:40 -0300

    asd

--- src/main/java/BCC/ES/CLP/config/DataLoader.java
+++ src/main/java/BCC/ES/CLP/config/DataLoader.java
@@ -1,6 +1,9 @@
 
 package BCC.ES.CLP.config;
 
+import BCC.ES.CLP.model.User;
+import BCC.ES.CLP.model.UserRole;
+import BCC.ES.CLP.repository.RepositoryUser;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +12,12 @@ import BCC.ES.CLP.model.Alvo;
 import BCC.ES.CLP.model.Scan;
 import BCC.ES.CLP.repository.RepositoryAlvo;
 import BCC.ES.CLP.repository.RepositoryScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class DataLoader {
     @Bean
-    CommandLineRunner loadData(RepositoryAlvo repo, RepositoryScan repoScan) {
+    CommandLineRunner loadData(RepositoryAlvo repo, RepositoryScan repoScan, RepositoryUser repositoryUser) {
     return new CommandLineRunner() {
         @Override
         public void run(String... args) throws Exception {
@@ -22,6 +26,8 @@ public class DataLoader {
             repo.save(new Alvo("www.stackoverflow.com", "45.33.32.156"));
             repoScan.save(new Scan(null, null, 80,"HTTP",google));
             repoScan.save(new Scan(null, null, 8080,"SSH",google));
+            String encryptedPassword = new BCryptPasswordEncoder().encode("123");
+            repositoryUser.save(new User(null,"usuario",encryptedPassword,"123", UserRole.ADMIN));
             }
         };
     }
--- src/main/java/BCC/ES/CLP/service/TokenService.java
+++ src/main/java/BCC/ES/CLP/service/TokenService.java
@@ -27,7 +27,6 @@ public class TokenService {
                     .sign(algorithm);
             return token;
         } catch (Exception e) {
-            e.printStackTrace();
             throw new RuntimeException("falha ao criar token");
         }
     }
